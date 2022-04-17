d9c1dede5a0cff0401961d17202417165aba69bd
jocelyntong9 <73870004+jocelyntong9@users.noreply.github.com>
2022-04-18 00:49:04 +0700

    Update edit_employee.php

--- permit/admin/master_data/employee/edit_employee.php
+++ permit/admin/master_data/employee/edit_employee.php
@@ -5,26 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
-    <link rel="stylesheet" href="employee_action1.css">
+    <link rel="stylesheet" href="employee_action.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>Edit Employee</title>
 </head>
 <body>
- <!-- mainbar -->
+    <!-- mainbar -->
     <div class="mainbar"> 
         <div class="a-logo"></div>
-        <button type="submit" class="logout">LOG OUT</button>
+        <nav>
+            <li><button type="submit" class="logout">LOG OUT</button></li>
+        </nav>
     </div> 
-
  <!-- sidebar -->
     <div class="a-sidebar">
             <c>Welcome, nama</c>
-            <button type="submit" class="a-menu"><a href="../../admin_dashboard/admin_dashboard.php">&ensp;Dashboard</a></button>               
-            <button type="submit" class="a-menu"><a href="../../admin_rol/admin_rol.php">&ensp;On Leave Requests</a></button>
-            <button type="submit" class="a-menu"><a href="../../admin_report/admin_report.php">&ensp;Report</a></button>
-            <button type="submit" class="a-open"><a href="employee_data.php">&ensp;Employee's Data</a></button>
-            <button type="submit" class="a-menu"><a href="../department/department_data.php">&ensp;Department's Data</a></button>
+            <button type="submit" class="a-menu"><a href="../../admin_dashboard/admin_dashboard.php">Dashboard</a></button>               
+            <button type="submit" class="a-menu"><a href="../../admin_rol/admin_rol.php">On Leave Requests</a></button>
+            <button type="submit" class="a-menu"><a href="../../admin_report/admin_report.php">Report</a></button>
+            <button type="submit" class="a-open"><a href="employee_data.php">Employee's Data</a></button>
+            <button type="submit" class="a-menu"><a href="../department/department_data.php">Department's Data</a></button>
     </div>
 <!-- kontent -->
     <div class="a-main">
@@ -63,6 +64,39 @@
                     </tr>
                 </table>
                 </div>
+                <?php
+                    include 'model_employee.php';
+                    $model = new Model();
+                    $id = $_REQUEST['id'];
+                    $row = $model->edit($id);
+
+                    if (isset($_POST['update'])) {
+                        if (isset($_POST['name']) && isset($_POST['department']) && isset($_POST['position']) && isset($_POST['contact'])&& isset($_POST['date_of_birth']) && isset($_POST['email']) && isset($_POST['gender'])) {                        
+                            if (!empty($_POST['name'])&& !empty($_POST['department']) && !empty($_POST['position']) && !empty($_POST['contact'])&& !empty($_POST['date_of_birth']) && !empty($_POST['email']) && !empty($_POST['gender'])) {     
+                                $data['id'] = $id;
+                                $data['name'] = $_POST['name'];
+                                $data['department'] = $_POST['department'];
+                                $data['position'] = $_POST['position'];
+                                $data['contact'] = $_POST['contact'];
+                                $data['date_of_birth'] = $_POST['date_of_birth'];
+                                $data['email'] = $_POST['email'];
+                                $data['gender'] = $_POST['gender'];
+
+                            $update = $model->update($data);
+
+                        if($update){
+                            echo "<script type='text/javascript'>alert('Employee has been edited!');window.location.href='employee_data.php';</script>";
+                        }else{
+                            echo "<script type='text/javascript'>alert('Please try again!');window.location.href='employee_data.php';</script>";
+                        }
+
+                        }else{
+                            echo "<script type='text/javascript'>alert('Empty data!');window.location.href='employee_data.php';</script>";
+                        }
+                    }
+                    }
+
+                    ?>
                 <div class="test2">
                     <form method="POST">
                     <table border="0" bordercolor="#c9c9c9" width="100%" cellspacing ="0" class="table2">
@@ -76,10 +110,11 @@
                     </tr>
                     <tr>
                        <td colspan="2" width= "350px">
-                            <input type="text" class="form" id="name" name="name" required>
+                            <input type="text" class="form" id="name" name="name" value="<?php echo $row['name']; ?>">
                         </td>
                         <td width= "350px">
-                            <input type="number" class="form" id="ID" name="ID" required>
+                            <input type="hidden" name="id" value="<?php echo $row['id'];?>" >
+                            <input type="number" class="form" id="id" name="id" value="<?php echo $row['id']; ?>" disabled>
                         </td>
                     </tr>
                     <tr>
@@ -100,13 +135,17 @@
                     </tr>
                     <tr>
                         <td width="130px">
-                            <input type="text" class="form1" id="department" name="department" required>
+                            <select class="form2" id="department" name="department">
+                                <option disabled selected><?php echo $row['department']; ?> </option>
+                                    <?php $all= new model(); $all->selectDepartment();?>
+                                </option>
+                            </select>
                         </td>
                         <td width="130px">
-                            <input type="text" class="form1" id="position" name="position" required>
+                            <input type="text" class="form1" id="position" name="position" value="<?php echo $row['position']; ?>" >
                         </td>
                         <td width= "350px">
-                            <input type="number" class="form" id="contact" name="contact" required>
+                            <input type="number" class="form" id="contact" name="contact"  value="<?php echo $row['contact']; ?>" >
                         </td>
                     </tr>
                     <tr>
@@ -124,10 +163,10 @@
                     </tr>
                     <tr>
                         <td colspan="2" width= "350px">
-                            <input type="date" class="form" id="dbo" name="dbo" required>
+                            <input type="date" class="form" id="date_of_birth" name="date_of_birth"  value="<?php echo $row['date_of_birth']; ?>" >
                         </td>
                         <td width= "350px">
-                            <input type="email" class="form" id="email" name="email" required>
+                            <input type="email" class="form" id="email" name="email"  value="<?php echo $row['email']; ?>" >
                         </td>
                     </tr>
                     <tr>
@@ -142,12 +181,16 @@
                     </tr>
                     <tr>
                         <td colspan="2" width= "350px">
-                            <input type="text" class="form" id="gender" name="gender" required>
+                            <select class="form3" id="gender" name="gender">
+                                    <option disabled selected><?php echo $row['gender']; ?> </option>
+                                    <?php $all= new model(); $all->selectGender();?>
+                                    </option>
+                            </select>
                         </td>
                     </tr>
                     <tr>
                         <td colspan="3">
-                            <button type="submit"class="save">Save</button>  
+                            <button type="submit" name="update" class="save">Save</button>  
                         </td>
                     </tr>
                 </table>
