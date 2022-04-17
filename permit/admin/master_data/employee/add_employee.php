689926d7687fe1a662ad95a0db5b8dce1686b14e
jocelyntong9 <73870004+jocelyntong9@users.noreply.github.com>
2022-04-18 00:48:23 +0700

    Update add_employee.php

--- permit/admin/master_data/employee/add_employee.php
+++ permit/admin/master_data/employee/add_employee.php
@@ -5,26 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
-    <link rel="stylesheet" href="employee_action1.css">
+    <link rel="stylesheet" href="employee_action.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>Add Employee</title>
 </head>
 <body>
- <!-- mainbar -->
+    <!-- mainbar -->
     <div class="mainbar"> 
         <div class="a-logo"></div>
-        <button type="submit" class="logout">LOG OUT</button>
+        <nav>
+            <li><button type="submit" class="logout">LOG OUT</button></li>
+        </nav>
     </div> 
-
  <!-- sidebar -->
     <div class="a-sidebar">
             <c>Welcome, nama</c>
-            <button type="submit" class="a-menu"><a href="../../admin_dashboard/admin_dashboard.php">&ensp;Dashboard</a></button>               
-            <button type="submit" class="a-menu"><a href="../../admin_rol/admin_rol.php">&ensp;On Leave Requests</a></button>
-            <button type="submit" class="a-menu"><a href="../../admin_report/admin_report.php">&ensp;Report</a></button>
-            <button type="submit" class="a-open"><a href="employee_data.php">&ensp;Employee's Data</a></button>
-            <button type="submit" class="a-menu"><a href="../department/department_data.php">&ensp;Department's Data</a></button>
+            <button type="submit" class="a-menu"><a href="../../admin_dashboard/admin_dashboard.php">Dashboard</a></button>               
+            <button type="submit" class="a-menu"><a href="../../admin_rol/admin_rol.php">On Leave Requests</a></button>
+            <button type="submit" class="a-menu"><a href="../../admin_report/admin_report.php">Report</a></button>
+            <button type="submit" class="a-open"><a href="employee_data.php">Employee's Data</a></button>
+            <button type="submit" class="a-menu"><a href="../department/department_data.php">Department's Data</a></button>
     </div>
 <!-- kontent -->
     <div class="a-main">
@@ -63,6 +64,13 @@
                     </tr>
                 </table>
                 </div>
+                <?php
+                    include 'model_employee.php';
+                    $model = new Model();
+
+                    $insert = $model->insert();
+                ?>
+
                 <div class="test2">
                     <form method="POST">
                     <table border="0" bordercolor="#c9c9c9" width="100%" cellspacing ="0" class="table2">
@@ -79,7 +87,7 @@
                             <input type="text" class="form" id="name" name="name" required>
                         </td>
                         <td width= "350px">
-                            <input type="number" class="form" id="ID" name="ID" required>
+                            <input type="number" class="form" id="id" name="id" required>
                         </td>
                     </tr>
                     <tr>
@@ -100,7 +108,11 @@
                     </tr>
                     <tr>
                         <td width="130px">
-                            <input type="text" class="form1" id="department" name="department" required>
+                        <select class="form2" id="department" name="department" required>
+                        <option disabled selected>  </option>
+                        <?php $all= new model(); $all->selectDepartment();?>
+                        </option>
+                        </select>
                         </td>
                         <td width="130px">
                             <input type="text" class="form1" id="position" name="position" required>
@@ -124,7 +136,7 @@
                     </tr>
                     <tr>
                         <td colspan="2" width= "350px">
-                            <input type="date" class="form" id="dbo" name="dbo" required>
+                            <input type="date" class="form" id="date_of_birth" name="date_of_birth" required>
                         </td>
                         <td width= "350px">
                             <input type="email" class="form" id="email" name="email" required>
@@ -142,12 +154,16 @@
                     </tr>
                     <tr>
                         <td colspan="2" width= "350px">
-                            <input type="text" class="form" id="gender" name="gender" required>
+                            <select class="form3" id="gender" name="gender" required>
+                                <option disabled selected>  </option>
+                                <?php $all= new model(); $all->selectGender();?>
+                                </option>
+                            </select>
                         </td>
                     </tr>
                     <tr>
                         <td colspan="3">
-                            <button type="submit"class="add">Add</button>  
+                            <button type="submit" name="submit" class="add">Add</button>  
                         </td>
                     </tr>
                 </table>
