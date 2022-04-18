9ab35d1074f49055a32ed3b3f21c3b09c5bb9ca6
Eric2031010 <73868408+Eric2031010@users.noreply.github.com>
2022-04-18 20:40:40 +0700

    Update CRUD.php

--- permit/class/CRUD.php
+++ permit/class/CRUD.php
@@ -2,46 +2,44 @@
 
 class CRUD extends database{
 
-public function insert( $table,$para=array() ){
-    $table_columns = implode( ',', array_keys( $para ));
-    $table_value = implode( "','", $para );
+    public function insert( $table,$para=array() ){
+        $table_columns = implode( ',', array_keys( $para ));
+        $table_value = implode( "','", $para );
 
-    $sql="INSERT INTO $table( $table_columns ) VALUES( '$table_value' )";
+        $sql="INSERT INTO $table( $table_columns ) VALUES( '$table_value' )";
 
-    $result = $this->mysqli->query($sql);
-}
-
-public function update( $table,$para=array(),$id ){
-    $args = array();
-
-    foreach ( $para as $key => $value ) {
-        $args[] = "$key = '$value'"; 
+        $result = $this->mysqli->query($sql);
     }
 
-    $sql="UPDATE  $table SET " . implode( ',', $args );
+    public function update( $table,$para=array(),$id ){
+        $args = array();
 
-    $sql .=" WHERE $id";
+        foreach ( $para as $key => $value ) {
+            $args[] = "$key = '$value'"; 
+        }
 
-    $result = $this->mysqli->query( $sql );
-}
+        $sql="UPDATE  $table SET " . implode( ',', $args );
 
-public function delete( $table,$id ){
-    $sql="DELETE FROM $table";
-    $sql .=" WHERE $id ";
-    $sql;
-    $result = $this->mysqli->query( $sql );
-}
+        $sql .=" WHERE $id";
 
-public function select($table,$rows="*",$where = null){
-    if ( $where != null ) {
-        $sql="SELECT $rows FROM $table WHERE $where";
-    }else{
-        $sql="SELECT $rows FROM $table";
+        $result = $this->mysqli->query( $sql );
     }
 
-    $this->sql = $result = $this->mysqli->query( $sql );
-}
+    public function delete( $table,$id ){
+        $sql="DELETE FROM $table";
+        $sql .=" WHERE $id ";
+        $sql;
+        $result = $this->mysqli->query( $sql );
+    }
 
+    public function select($table,$rows="*",$where = null){
+        if ( $where != null ) {
+            $sql="SELECT $rows FROM $table WHERE $where";
+        }else{
+            $sql="SELECT $rows FROM $table";
+        }
 
+        $this->sql = $result = $this->mysqli->query( $sql );
+    }
 
 }
