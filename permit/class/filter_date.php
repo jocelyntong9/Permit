c2503c987759fb1b9b7f9b6e4b3b47282652506a
Eric2031010 <73868408+Eric2031010@users.noreply.github.com>
2022-04-18 20:29:59 +0700

    Update filter_date.php

--- permit/class/filter_date.php
+++ permit/class/filter_date.php
@@ -2,11 +2,11 @@
 
 class filter_date extends database implements filter{
     
-    public function filter($table,$rows="*",$date1,$date2,$id){
+    public function filter( $table,$rows="*",$date1,$date2,$id ){
         $sql = "SELECT $rows FROM $table 
         WHERE id='$id' 
-        AND ((on_leave_from between '$date1' and '$date2') 
-        OR  (on_leave_to between '$date1' and '$date2'))";
+        AND (( on_leave_from between '$date1' and '$date2' ) 
+        OR  ( on_leave_to between '$date1' and '$date2' ))";
 
         
         $this->sql = $result = $this->mysqli->query($sql);
