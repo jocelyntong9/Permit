94c5a3bfcff6166db736b3b7999ac7c28dede4e8
jocelyntong9 <73870004+jocelyntong9@users.noreply.github.com>
2022-04-30 23:40:00 +0700

    Update add_employee.php

--- permit/admin/master_data/employee/add_employee.php
+++ permit/admin/master_data/employee/add_employee.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://code.iconify.design/2/2.0.4/iconify.min.js"></script>
-    <link rel="stylesheet" href="employee_action.css">
+    <link rel="stylesheet" href="employee_action1.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>Add Employee</title>
@@ -36,43 +36,35 @@
         <div class="content"> 
             <fieldset>
                 <div class="test">
-                <table border="0" bordercolor="#c9c9c9" width="100%" cellspacing ="0" class="table1">
-                    <tr>
-                       <td>
-                            <div class="profile"></div>
-                        </td>
-                    </tr>
-                    <tr>
-                       <td>
-                            Profile Picture
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <br>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <button type="submit" class="upload">Upload Photo</button>  
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <button type="submit" class="remove">Remove Photo</button>  
-                        </td>
-                    </tr>
-                </table>
-                </div>
                 <?php
                     include 'model_employee.php';
                     $model = new Model();
 
                     $insert = $model->insert();
                 ?>
+                <form method="POST" enctype="multipart/form-data">
+                <table border="0" bordercolor="#c9c9c9" width="100%" cellspacing ="0" class="table1">
+                    <tr>
+                       <td>
+                            <div>
+                                <image class="profile" src="../../../image/avatar.png"/>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                       <td style="padding-bottom: 10px">
+                            Profile Picture
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input class="photo" id="photo" name="photo" type="file" accept=".jpg,.jpeg,.png">
+                        </td>
+                    </tr>
+                </table>
+                </div>
 
                 <div class="test2">
-                    <form method="POST">
                     <table border="0" bordercolor="#c9c9c9" width="100%" cellspacing ="0" class="table2">
                     <tr>
                        <td colspan="2" width= "350px">
@@ -83,16 +75,11 @@
                         </td>
                     </tr>
                     <tr>
-                       <td colspan="2" width= "350px">
+                       <td colspan="2" width= "350px" style="padding-bottom: 20px">
                             <input type="text" class="form" id="name" name="name" required>
                         </td>
-                        <td width= "350px">
+                        <td width= "350px" style="padding-bottom: 20px">
                             <input type="number" class="form" id="id" name="id" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" width= "350px">
-                            <br>
                         </td>
                     </tr>
                     <tr>
@@ -107,23 +94,18 @@
                         </td>
                     </tr>
                     <tr>
-                        <td width="130px">
+                        <td width="130px" style="padding-bottom: 20px">
                         <select class="form2" id="department" name="department" required>
-                        <option disabled selected>  </option>
-                        <?php $all= new model(); $all->selectDepartment();?>
-                        </option>
+                            <option disabled selected>  </option>
+                                <?php $all= new model(); $all->selectDepartment();?>
+                            </option>
                         </select>
                         </td>
-                        <td width="130px">
+                        <td width="130px" style="padding-bottom: 20px">
                             <input type="text" class="form1" id="position" name="position" required>
                         </td>
-                        <td width= "350px">
+                        <td width= "350px" style="padding-bottom: 20px">
                             <input type="number" class="form" id="contact" name="contact" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" width= "350px">
-                            <br>
                         </td>
                     </tr>
                     <tr>
@@ -135,16 +117,11 @@
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="2" width= "350px">
+                        <td colspan="2" width= "350px" style="padding-bottom: 20px">
                             <input type="date" class="form" id="date_of_birth" name="date_of_birth" required>
                         </td>
-                        <td width= "350px">
+                        <td width= "350px" style="padding-bottom: 20px">
                             <input type="email" class="form" id="email" name="email" required>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2" width= "350px">
-                            <br>
                         </td>
                     </tr>
                     <tr>
@@ -153,7 +130,7 @@
                         </td>
                     </tr>
                     <tr>
-                        <td colspan="2" width= "350px">
+                        <td colspan="2" width= "350px" style="padding-bottom: 22px">
                             <select class="form3" id="gender" name="gender" required>
                                 <option disabled selected>  </option>
                                 <?php $all= new model(); $all->selectGender();?>
