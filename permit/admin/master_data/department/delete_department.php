646559e51932d77b3e4ba1cafaa211a0bcc384a6
Eric2031010 <73868408+Eric2031010@users.noreply.github.com>
2022-05-03 22:56:06 +0700

    Add files via upload

---
+++ permit/admin/master_data/department/delete_department.php
@@ -0,0 +1,13 @@
+<?php 
+
+	include '../../../class/init.php';
+	$a = new CRUD();
+	$id = $_REQUEST['id'];
+	
+	$a -> delete('department',"id='$id'");
+
+	if ($a==true) {
+        echo "<script type='text/javascript'>alert('Department has been deleted!');window.location.href='department_data.php';</script>";
+	}
+
+ ?>
